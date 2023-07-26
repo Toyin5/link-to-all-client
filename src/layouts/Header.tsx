2afc73f8d919b824ffdfc855baa8e5cffe51540f
Toyin Muhammed <toyinmuhammed50@gmail.com>
2023-07-26 12:55:13 +0200

    hotfix: hamburger

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -68,20 +68,13 @@ function Header() {
                     onClick={() => setOpenNav(!openNav)}
                 >
                     {openNav ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            className="h-6 w-6"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                             <path
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                d="M6 18L18 6M6 6l12 12"
-                            />
+                                d="M3.75 9h16.5m-16.5 6.75h16.5" />
                         </svg>
+
                     ) : (
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
